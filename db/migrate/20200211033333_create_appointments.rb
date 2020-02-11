6ba7e0ee25fdd639a2ba55590d409010860b9112
Elana Ryznar <eryzna@gmail.com>
2020-02-11 03:49:30 +0000

    Automatically backed up by Learn

--- db/migrate/20200211033333_create_appointments.rb
+++ db/migrate/20200211033333_create_appointments.rb
@@ -1,9 +1,9 @@
 class CreateAppointments < ActiveRecord::Migration[5.0]
   def change
     create_table :appointments do |t|
-      t.datetime :appointments_datetime
-      t.belongs_to :doctors
-      t.belongs_to :patients
+      t.datetime :appointment_datetime
+      t.belongs_to :doctor
+      t.belongs_to :patient
 
       t.timestamps null: false
     end
